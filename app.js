695a9a64f4070380568eda83abf48413cd6e430c
joelpdurham <joeldur@gmail.com>
2019-09-30 21:38:40 -0700

    the js works to check for correct right answers, display answers, and score.

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
 const myButton = document.getElementById('action');
 
+var score = 0
+
 function isYesOrNo(userAnswer) {
     const lowerCaseAnswer = userAnswer.toLowerCase()
-    if (lowerCaseAnswer.includes('y',) === true) {
+    if (lowerCaseAnswer.startsWith('y') === true) {
         return true;
-    } else if (lowerCaseAnswer.includes('n') === true) {
+    } else if (lowerCaseAnswer.startsWith('n', 0) === true) {
         return false;
     }
 }
 
+
+function isCorrect(userAnswer, actualAnswer, answerBox) {
+    if (isYesOrNo(userAnswer) === actualAnswer) {
+        score += 10;
+        answerBox.style.color = '#00b01a';
+    } else {
+        answerBox.style.color = '#d60000';
+    }
+}
+
 myButton.onclick = () => {
-    alert('welcome to my quiz');
-    const myConfirmation = confirm('do you want to take my quiz?')
+    const myConfirmation = confirm('Do you want to take my quiz?');
 
     if (myConfirmation === true) {
 
-        const userAnswer = prompt('Questions 1');
+        aboutme.style.display = "none"
 
-        const sanitizedUserAnswer = isYesOrNo(userAnswer)
+        const answer1 = prompt('Do I like boooooooks?');
+        const answer1Box = document.getElementById('answer1');
+        answer1Box.textContent = answer1;
+        isCorrect(answer1, true, answer1Box);
+
+        const answer2 = prompt('I have read about 45 books this year.');
+        const answer2Box = document.getElementById('answer2');
+        answer2Box.textContent = answer2;
+        isCorrect(answer2, false, answer2Box);
+
+        const answer3 = prompt('I was a touring actor.');
+        const answer3Box = document.getElementById('answer3');
+        answer3Box.textContent = answer3;
+        isCorrect(answer3, true, answer3Box);
+
+        //Display Score
+        const scoreBox = document.getElementById('score');
+        scoreBox.textContent = score;
+
+        results.style.display = "block"
+
+        //reveal hidden section
+
+        //make something that jumps to the next section
+        
 
     }
 }
